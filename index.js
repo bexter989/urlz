86520925401371d1cce6987138408355d49b3339
Bernard Solien <solienbernard989@gmail.com>
2020-10-03 15:54:49 +1000

    front-end & tweaks in index.js

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup')
+const monk = require('monk')
+let { nanoid } = require('nanoid')
+
+require('dotenv').config();
+
+let db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+
+urls.createIndex(
+    { slug: 1 },
+    { unique: true }
+);
+
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
@@ -16,14 +32,66 @@ app.get('/', (req,res) => {
     // TODO: redirect to Url
 });
 
-app.get('/url/:id', (req,res) => {
-    // TODO: get a short url by id
+
+app.get('/:id', async (req,res) => {
+    const { id: slug } = req.params;
+    try {
+        const url = await urls.findOne({ slug });
+        if (url){
+            res.redirect(url.url);
+        }
+        res.redirect(`/?error=${slug} not found`)
+    } catch (error) {
+        res.redirect(`/?error=Link not found`);
+    }
 });
 
-app.post('/url', (req,res) => {
-    // TODO: Create a short Url
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+})
+app.post('/url', async (req, res, next) => {
+    let { slug, url }  = req.body;
+    try {
+        await schema.validate({
+            slug,
+            url,
+        })
+        if (!slug){
+            slug = nanoid(5);
+        }
+        // else {
+        //     const existing = await urls.findOne({ slug });
+        //     if (existing) {
+        //         throw new Error('Slug in Use! :-(');
+        //     }
+        // }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error){
+        if (error.message.startsWith('E11000')) {
+            error.message = 'Slug already used! (oO)'
+        }
+        next(error)
+    }
 });
 
+app.use((error, req, res, next) => {
+    if (error.status){
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        messaege: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '***' : error.stack,
+    })
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
